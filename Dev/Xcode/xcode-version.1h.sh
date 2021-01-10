30cefce9946bfa2320906a14cd5545e41229bdfa
Makoto OTSUKA <kotowo@mac.com>
2021-01-10 11:07:49 +0900

    SF Symbols のアイコンを追加してみた

--- Dev/Xcode/xcode-version.1h.sh
+++ Dev/Xcode/xcode-version.1h.sh
@@ -5,7 +5,6 @@
 # <bitbar.author.github>hirschfl</bitbar.author.github>
 # <bitbar.desc>Shows the Xcode version that is currently selected with xcode-select.</bitbar.desc>
 # <bitbar.dependencies>xcodebuild</bitbar.dependencies>
-
-xcode=$(xcodebuild -version | head -1)
-build=$(xcodebuild -version | grep -i "build version" | awk '{print $3}')
-echo "${xcode}(${build})"
+version=$(xcodebuild -version | head -1 | awk '{print $1}')
+build=$(xcodebuild -version | grep -i "bu|ild version" | awk '{print $3}')
+echo "􀤊${version}(${build})"
